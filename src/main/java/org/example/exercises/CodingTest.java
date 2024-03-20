584542f0940e9dda9d8bff5dd38f133e6ca51d6a
Felipesdr <felipederose@gmail.com>
2024-03-20 10:24:20 -0300

    Add exercise4 method in CodingTest.java.

--- src/main/java/org/example/exercises/CodingTest.java
+++ src/main/java/org/example/exercises/CodingTest.java
@@ -62,5 +62,17 @@ public class CodingTest {
         System.out.println("d) 1, 1, 2, 3, 5, 8, 13");
     }
 
+    public void exercise4(){
 
+        System.out.println("Vamos chamar as sala de 1, 2 e 3 e os interruptores de A,B e C");
+        System.out.println("Primeiramente, eu apertaria dois interruptores (a e b, por exemplo), e iria na sala 3.");
+        System.out.println("Caso a luz da sala 3 estivesse apagada, o responsavel por ela seria o interruptor C" +
+                "e bastaria acender o interruptor a e ir para a sala 1." +
+                "Caso a luz estivesse acesa, seria interruptor a = 1, b = 2, c = 3. Caso estivesse apagada, a = 2, b = 1 e c = 3." +
+                "o interruptor a seria responsavel pela sala 2  e o interruptor b pela sala 1.");
+        System.out.println("Agora, voltando à primeira tentativa, caso a luz estivesse acesa, eu apertaria os interruptores A e C e iria na sala 2" +
+                ". Caso a luz estivesse acesa, Saberiamos que a = 2, b = 3 e c = 1, e caso estivesse apagada, a = 1, c = 2 e b = 3.");
+
+
+    }
 }
