92247b5eb7fd2827beab8ab7cce6c27dceb7bb87
Felipesdr <felipederose@gmail.com>
2024-03-20 09:12:14 -0300

    Add function exercise1 to class CodingTest

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.exercises.CodingTest;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CodingTest codingTest = new CodingTest();
+
+        int resultEx1 = codingTest.exercise1(13);
+
+        System.out.println("The result of exercise 1 is:" + resultEx1);
+    }
+}
---
+++ src/main/java/org/example/exercises/CodingTest.java
@@ -0,0 +1,17 @@
+package org.example.exercises;
+
+public class CodingTest {
+
+    public Integer exercise1(int i){
+        int k = 0;
+        int soma = 0;
+
+        while(k < i){
+
+            k++;
+            soma += k;
+        }
+
+        return soma;
+    }
+}
